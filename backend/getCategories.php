953b8bb8fd8f3c900290404d517bcf68b22cff66
Zakaria YAHYA <zakariayahya456@gmail.com>
2025-01-02 21:06:05 +0200

    Did some changes in the getCategories.php file

--- backend/getCategories.php
+++ backend/getCategories.php
@@ -1,13 +1,13 @@
 <?php
 try {
     require_once "connection.php";
-    //Start a session to check $_SESSION['AdminName']
-    // session_id($data["sessionId"] ?? "");
-    // session_start();
-    // if (!isset($_SESSION['AdminName']) || empty($_SESSION['AdminName'])) {
-    //     echo json_encode(["message" => "Error: You are not logged in", "success" => false]);
-    //     exit;
-    // }
+    // Start a session to check $_SESSION['AdminName']
+    session_id($data["sessionId"] ?? "");
+    session_start();
+    if (!isset($_SESSION['AdminName']) || empty($_SESSION['AdminName'])) {
+        echo json_encode(["message" => "Error: You are not logged in", "success" => false]);
+        exit;
+    }
     $stmt = $conn->prepare("SELECT * FROM categories");
     $stmt->execute();
     $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
